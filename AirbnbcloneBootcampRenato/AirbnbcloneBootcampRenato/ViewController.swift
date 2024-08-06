fd9d99e54e0d1b60ee9f4ecc5efc6c5409cc406e
vierrra <vierrra@hotmail.com>
2024-08-05 22:05:12 -0300

    CReate project

---
+++ AirbnbcloneBootcampRenato/AirbnbcloneBootcampRenato/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  AirbnbcloneBootcampRenato
+//
+//  Created by Renato Vieira on 05/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
